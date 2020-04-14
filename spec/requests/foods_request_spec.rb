dd235594383038ead3e92c3dfc5232f70af21443
Benjamin Rosas <ben@aliencyb.org>
2020-04-14 01:07:15 -0500

    A grip of Gems and testing improvements.

--- spec/requests/foods_request_spec.rb
+++ spec/requests/foods_request_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Foods Request', type: :request do
-  it 'returns an empty collection when there are no foods' do
-    get '/foods'
+RSpec.describe "Foods Request", type: :request do
+  it "returns an empty collection when there are no foods" do
+    get "/foods"
 
     expect(response).to have_http_status(:success)
-    expect(response.content_type).to eq('application/vnd.api+json')
+    expect(response.content_type).to eq("application/vnd.api+json")
 
     response_hash = JSON.parse(response.body).deep_symbolize_keys
     expect(response_hash).to be_a(Hash)
@@ -15,27 +15,24 @@ RSpec.describe 'Foods Request', type: :request do
     expect(response_hash[:data]).to be_empty
   end
 
-  it 'returns a collection of foods when there are foods' do
-    apples = Food.create(name: 'apples')
-    bananas = Food.create(name: 'bananas')
-    chocolate = Food.create(name: 'chocolate')
+  it "returns a collection of foods when there are foods" do
+    create_list(:food, 3)
 
-    get '/foods'
+    get "/foods"
 
     expect(response).to have_http_status(:success)
-    expect(response.content_type).to eq('application/vnd.api+json')
+    expect(response.content_type).to eq("application/vnd.api+json")
 
     response_hash = JSON.parse(response.body).deep_symbolize_keys
     expect(response_hash).to be_a(Hash)
     expect(response_hash).to have_key(:data)
     expect(response_hash[:data].length).to eq(3)
+    expect(response_hash).to_not have_key(:errors)
 
-    attrs = response_hash[:data].map { |item| item[:attributes] }
-    expect(attrs).to contain_exactly(
-      { name: 'apples' }, { name: 'bananas' }, { name: 'chocolate' }
-    )
+    attrs = response_hash[:data].map { |item| item[:attributes][:name] }
+    expect(attrs).to contain_exactly(*Food.all.map(&:name))
 
     ids = response_hash[:data].map { |item| item[:id].to_i }
-    expect(ids).to contain_exactly(apples.id, bananas.id, chocolate.id)
+    expect(ids).to contain_exactly(*Food.all.map(&:id))
   end
 end
